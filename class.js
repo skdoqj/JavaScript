7086c9cda42e19b9207f110d5a1717ac1ff4c56c
suwan <skdoqj@naver.com>
2023-06-16 01:53:47 +0900

    12강 상속,super

--- class.js
+++ class.js
@@ -7,8 +7,23 @@ class factory {
     return this.first + this.second;
   }
 }
+//상속
+class factoryPlus extends factory {
+  constructor(f, s, t) {
+    //부모인자 호출
+    super(f, s);
+    this.third = t;
+  }
+  sum() {
+    //부모 메서드 값 호출
+    return super.sum() + this.third;
+  }
+  avg() {
+    return (this.first + this.second + this.third) / 3;
+  }
+}
 
-const A = new factory(10, 20);
-const B = new factory(20, 20);
-console.log("A", A.sum());
-console.log("B", B.sum());
+const A = new factoryPlus(10, 20, 30);
+const B = new factoryPlus(20, 20);
+console.log("A", A.sum(), A.avg());
+console.log("B", B.sum(), B.avg());
